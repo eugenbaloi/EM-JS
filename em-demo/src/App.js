66f124c8bcdd679d44067d436a746d795f3fceb8
eugenbaloi <55147070+eugenbaloi@users.noreply.github.com>
2020-01-07 20:02:48 +0200

    React

---
+++ em-demo/src/App.js
@@ -0,0 +1,47 @@
+  import React, { Component } from 'react';
+  import {
+    BrowserRouter as Router,
+    Link,
+    Switch,
+    Route
+  } from 'react-router-dom';
+  import { Home } from './Pages/Home/Home';
+
+  export class App extends Component {
+    constructor(props) {
+      super(props);
+
+      this.state= {
+        name: 'Eugen',
+        surname: 'Baloi',
+      };
+    }
+
+      render() {
+        return (
+          <Router>
+            <nav>
+              <ul>
+                <li>
+                  <Link to ="/">Home</Link>
+                </li>
+                <li>
+                <Link to ="/about">About</Link>
+                </li>
+              </ul>
+            </nav>
+            <hr></hr>
+      
+            <Switch>
+            <Route exact path="/">
+              <h1>Home</h1>
+            <Home name="Salam"></Home>
+            </Route>
+            <Route path="/about">
+            <h1>About</h1>
+            </Route>
+            </Switch>
+          </Router>
+        );
+    }
+  }
